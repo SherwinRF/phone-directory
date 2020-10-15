3c1080b34a3c7d3960b7e20c8c67fc23fa070fbb
Sherwin Fernandes <sherwinrf11@gmail.com>
2020-10-15 17:23:48 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Header from './Header'
 import './App.css';
 class App extends Component {
+
+
+
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Sherwin Fernandes",
+        phone: "1234567890"
+      },
+      {
+        id: 2,
+        name: "Shilpa Bhat",
+        phone: "0987654321"
+      }
+    ]
 
   return (
     <div >
@@ -14,6 +30,16 @@ class App extends Component {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+              <span className="grid-item"> {sub.name} </span>
+              <span className="grid-item"> {sub.phone} </span>
+            </div>
+            })
+          }
+
       </div>
 
     </div>
